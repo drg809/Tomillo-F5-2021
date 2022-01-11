cf6ecaac1aa176daf193bf43e610b2b9061c975c
drg809 <drg809@gmail.com>
2022-01-11 14:35:52 +0100

    distintas soluciones para la funcion de operacion

--- Modulos/Modulo2/Contador/main.js
+++ Modulos/Modulo2/Contador/main.js
@@ -8,14 +8,45 @@ window.onload = function() {
    var btnRes = document.getElementById("btnRes");
    var btnAdd = document.getElementById("btnAdd");
    
-   function subtraction() {
-      counter -= 1;
+   function op(num) {
+      // Solución 1
+      counter += num;
       counterElement.innerHTML = counter;
-   }
-
-   function add() {
-      counter += 1;
-      counterElement.innerHTML = counter;
+      // Solución 2
+      // if(num == 2) {
+      //    counter -= 1;
+      // }else if(num == 1) {
+      //    counter += 1;
+      // }else if(num == 3) {
+      //    counter *= 2;
+      // }else if(num == 4) {
+      //    counter = counter / 2;
+      // }else if(num == 5) {
+      //    counter = Math.sqrt(counter);
+      // }else{
+      //    counter = 0;
+      // }
+      // Solución 3
+      // switch (num) {
+      //    case 2:
+      //       counter -= 1;
+      //       break;
+      //    case 3:
+      //       counter *= 2;
+      //       break;
+      //    case 4:
+      //       counter = counter / 2;
+      //       break;
+      //    case 5:
+      //       counter = Math.sqrt(counter);
+      //       break;
+      //    case 1:
+      //       counter += 1;
+      //       break;
+      //    default:
+      //       counter = 0;
+      //       break;
+      // }
    }
 
    function reset() {
@@ -23,7 +54,11 @@ window.onload = function() {
       counter = 0;
    }
 
-   btnSub.addEventListener("click", subtraction);
-   btnAdd.addEventListener("click", add);
+   btnSub.addEventListener("click", function() {
+      op(-1);
+   });
+   btnAdd.addEventListener("click", function() {
+      op(1);
+   });
    btnRes.addEventListener("click", reset);
 }
