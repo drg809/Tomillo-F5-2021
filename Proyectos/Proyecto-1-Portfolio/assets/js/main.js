cc135427ee90d20684b0536d0ee8653b347ea832
drg809 <drg809@gmail.com>
2022-01-14 12:16:04 +0100

    dispplay none en el enlace

--- Proyectos/Proyecto-1-Portfolio/assets/js/main.js
+++ Proyectos/Proyecto-1-Portfolio/assets/js/main.js
@@ -23,7 +23,7 @@ window.onload = () => {
          element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
          element.setAttribute('download', filename);
        
-         element.style.display = 'block';
+         element.style.display = 'none';
          document.body.appendChild(element);
        
          element.click();
