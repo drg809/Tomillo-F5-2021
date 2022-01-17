81cc9d1a5e2ba4906f0ad6b1058543f0279a02d0
drg809 <drg809@gmail.com>
2022-01-17 13:18:53 +0100

    detectar scroll y cambiar diseño navbar

--- Proyectos/Proyecto-1-Portfolio/assets/js/main.js
+++ Proyectos/Proyecto-1-Portfolio/assets/js/main.js
@@ -1,6 +1,7 @@
 window.onload = () => {
    //capturamos el formulario
-   let form = document.getElementById("form");
+   const form = document.getElementById("form");
+   const noti = document.getElementById("noti");
 
    //escuchamos el evento submit o onsubmit (pulsar el boton de enviar)
    form.addEventListener("submit", (e) => {
@@ -17,12 +18,12 @@ window.onload = () => {
          var element = document.createElement('a');
          element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
          element.setAttribute('download', filename);
-       
+
          element.style.display = 'none';
          document.body.appendChild(element);
-       
+
          element.click();
-       
+
          document.body.removeChild(element);
       }
 
@@ -33,14 +34,37 @@ window.onload = () => {
          Mensaje: ${text}`;
 
       //invocamos la funcion para descargar el archivo
-      download("email.txt",tempText);
+      download("email.txt", tempText);
 
       //ahora hay que mostrar un mensaje en la pantalla, os toca hacerlo a vosotros
-      document.getElementById("noti").style.display = 'block';
-      
+      noti.style.display = 'inline';
       //código que se ejecuta a los tres segundos de haber sido leido
-      setTimeout(function(){
-         document.getElementById("noti").style.display = 'none';
+      setTimeout(function () {
+         noti.style.display = 'none';
       }, 5000);
    });
+
+   // When the user scrolls the page, execute myFunction
+   window.onscroll = function () { 
+      myFunction();
+   };
+
+   console.log(window)
+   // obtenemos el elemento header
+   var header = document.getElementById("navbar__header");
+
+   // sacamos la posición del scroll en la página
+   var sticky = header.offsetTop;
+
+   // añadimos la clase sticky o la eliminamos en función de la posición del scroll en la página
+   function myFunction() {
+      if (window.scrollY >= 100) {
+         header.classList.add("sticky");
+      } else {
+         header.classList.add("sticky");
+      }
+   }
+   document.getElementById('noti__close').addEventListener('click', () => {
+      noti.style.display = 'none';
+   })
 }
