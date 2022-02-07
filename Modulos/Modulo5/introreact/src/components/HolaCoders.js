ccd98690aac2e292bc6ec25485b680f1ce24968d
drg809 <drg809@gmail.com>
2022-02-07 14:56:58 +0100

    funciones añadidas al react basico y ejemplos con props

--- Modulos/Modulo5/introreact/src/components/HolaCoders.js
+++ Modulos/Modulo5/introreact/src/components/HolaCoders.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+
+function hola(){
+   console.log('funcion hola');
+}
+const hola1 = () => {
+   console.log('funcion flecha');
+}
+
 const HolaCoders = () => {
+   hola1();
+   hola();
+   console.log('hola coders');
    return (
       <>
-         <div>Hola Coders</div>
+         <div>Hola</div>
          <p>Introducción a React</p>
       </>
    );
